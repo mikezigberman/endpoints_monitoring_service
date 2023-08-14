a51f9155e9890813481da8afe51dec829c46d35f
Mike Zigberman <mikezigberman@yahoo.com>
2023-08-14 18:40:13 +0200

    ems: project creation

---
+++ src/main/java/com/example/endpointsmonitoringservice/EndpointsMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.endpointsmonitoringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointsMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointsMonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/endpointsmonitoringservice/EndpointsMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endpointsmonitoringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointsMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
